127623606751a6e303d6f0bda75aafb9ca8b115d
Christopher Newton <christopher.k.newton@me.com>
2016-06-19 12:17:58 +0200

    Add hue min and max range

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var hslToHex = require('tie-dye/hslToHex');
 
-function hashbow(input, saturation, lightness) {
+function hashbow(input, saturation, lightness, hueRangeMin, hueRangeMax) {
 
-  var inputAsString, sum;
+  var inputAsString, sum, hue;
   saturation = saturation || 50;
   lightness = lightness || 50;
+  hueRangeMin = (hueRangeMin < 360 && hueRangeMin > 0 ? hueRangeMin : 0) || 0;
+  hueRangeMax = (hueRangeMax < 360 && hueRangeMax > 0 ? hueRangeMax : 360) || 360;
 
   var greyValues = [null, undefined, [], {}, '', new RegExp()];
 
@@ -40,8 +42,9 @@ function hashbow(input, saturation, lightness) {
   }
 
   sum = Math.abs(sum * sum);
+  hue = (sum % (hueRangeMax - hueRangeMin)) + hueRangeMin;
 
-  return hslToHex(sum % 360, saturation, lightness);
+  return hslToHex(hue, saturation, lightness);
 }
 
 module.exports = hashbow;
